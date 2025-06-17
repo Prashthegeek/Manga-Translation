1bb77ea73a0386f2062452cf15a6426a631a3d5a
Prasthegeek <sharmaprashant9763@gmail.com>
2025-06-17 14:18:56 +0530

    dockerized+ made output better

--- backend/server.js
+++ backend/server.js
@@ -1715,8 +1715,23 @@ app.use(cors());
 app.use(express.json());
 
 // Initialize Google Cloud clients
-const visionClient = new ImageAnnotatorClient({ keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS });
-const translationClient = new TranslationServiceClient({ keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS });
+
+// Construct the credentials object using process.env
+const credentials = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key, 
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain:process.env.universe_domain
+};
+const visionClient = new ImageAnnotatorClient({credentials });
+const translationClient = new TranslationServiceClient({credentials });
 
 const projectId = process.env.GOOGLE_CLOUD_PROJECT_ID;
 const location = 'global';
